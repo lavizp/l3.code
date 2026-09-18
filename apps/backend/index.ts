556da02e263b6171bb76a38cf69b9460137137e0
lavizp <pandeylaviz@gmail.com>
2026-09-19 00:09:22 +0545

    feat: catch db connection errors

--- apps/backend/index.ts
+++ apps/backend/index.ts
@@ -2,15 +2,19 @@ import { WebSocketServer } from "ws";
 import mongoose from "mongoose"
 import { WorkspaceModel } from "db/client";
 mongoose.connect(process.env.DB_URL!)
-
-const server = new WebSocketServer({ port: 8080 })
-console.log('started')
-server.on("connection", (ws) => {
-  ws.on("message", (msg) => {
-    console.log(msg)
-    WorkspaceModel.create({
-      path: '123',
-      name: '1234'
+  .then(() => {
+    const server = new WebSocketServer({ port: 8080 })
+    console.log('started')
+    server.on("connection", (ws) => {
+      ws.on("message", (msg) => {
+        console.log(msg)
+        WorkspaceModel.create({
+          path: '123',
+          name: '1234'
+        })
+      })
     })
   })
-})
+  .catch(e => {
+    console.log(e)
+  })
